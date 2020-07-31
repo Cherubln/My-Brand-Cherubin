4c87bf646dbca401db61e470c3753b9d5dcfdd3a
Cherubin <cherubinkubwayo@gmail.com>
2020-07-31 10:27:17 +0200

    ft-contact-form: form data store

---
+++ UI/ownerBlogPage/ownerBlog.js
@@ -0,0 +1,37 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAyuyxcth2vf-Cxp911GxUX25i1WCyp0Qg",
+  authDomain: "fir-my-brand.firebaseapp.com",
+  databaseURL: "https://fir-my-brand.firebaseio.com",
+  projectId: "fir-my-brand",
+  storageBucket: "fir-my-brand.appspot.com",
+  messagingSenderId: "828238082301",
+  appId: "1:828238082301:web:7daae22b54f73cef9b8e94",
+  measurementId: "G-1Q52GL66BN",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const messages = document.querySelector("#messages");
+
+function contactFormData(doc) {
+  const name = document.createElement("h4");
+  const email = document.createElement("p");
+  const message = document.createElement("p");
+
+  name.textContent = doc.data().Name;
+  email.textContent = doc.data().Email;
+  message.textContent = `${doc.data().Name} said: ${doc.data().Message}`;
+
+  messages.appendChild(name);
+  messages.appendChild(email);
+  messages.appendChild(message);
+}
+
+db.collection("contact-form")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      contactFormData(doc);
+    });
+  });
