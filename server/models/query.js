e190c47ed0be803ef80620a71f88764deccc1e0f
Cherubin <cherubinkubwayo@gmail.com>
2020-08-12 16:00:38 +0200

    ft-store-queries: validate query

--- server/models/query.js
+++ server/models/query.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
-const Joi = require("joi");
 
 const schema = new mongoose.Schema({
-  name: Joi.string().min(3).required(),
-  email: Joi.string().email().required(),
+  name: String,
+  email: String,
 });
 
 module.exports = mongoose.model("query", schema);
