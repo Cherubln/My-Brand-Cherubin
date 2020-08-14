20a7bace1ebf24f7d6d7c009cb576eb7dc4615f2
Cherubin <cherubinkubwayo@gmail.com>
2020-08-14 14:49:44 +0200

    ft-crud: add crud op

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const Blog = require("../models/blog");
 const router = express.Router();
+const verifyToken = require("./verifyToken");
+const jwt = require("jsonwebtoken");
 
 router.get("/blogs", async (req, res) => {
   const blogs = await Blog.find();
   res.send(blogs);
 });
 
-router.post("/blogs", async (req, res) => {
-  const blog = new Blog({
-    title: req.body.title,
-    content: req.body.content,
+router.post("/blogs", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (error) => {
+    if (error) {
+      res.sendStatus(403);
+    } else {
+      const blog = new Blog({
+        title: req.body.title,
+        content: req.body.content,
+      });
+      blog.save();
+      res.send(blog);
+    }
   });
-  await blog.save();
-  res.send(blog);
 });
 
 router.get("/blogs/:id", async (req, res) => {
@@ -26,34 +34,46 @@ router.get("/blogs/:id", async (req, res) => {
   }
 });
 
-router.patch("/blogs/:id", async (req, res) => {
-  try {
-    const blog = await Blog.findOne({ _id: req.params.id });
+router.patch("/blogs/:id", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (error) => {
+    if (error) {
+      res.sendStatus(403);
+    } else {
+      try {
+        const blog = Blog.findOne({ _id: req.params.id });
 
-    if (req.body.title) {
-      blog.title = req.body.title;
+        if (req.body.title) {
+          blog.title = req.body.title;
+        }
+
+        if (req.body.content) {
+          blog.content = req.body.content;
+        }
+
+        blog.save();
+        res.send(blog);
+      } catch {
+        res.status(404);
+        res.send({ error: "blog doesn't exist!" });
+      }
     }
-
-    if (req.body.content) {
-      blog.content = req.body.content;
-    }
-
-    await blog.save();
-    res.send(blog);
-  } catch {
-    res.status(404);
-    res.send({ error: "blog doesn't exist!" });
-  }
+  });
 });
 
-router.delete("/blogs/:id", async (req, res) => {
-  try {
-    await Blog.deleteOne({ _id: req.params.id });
-    res.status(204).send();
-  } catch {
-    res.status(404);
-    res.send({ error: "blog doesn't exist!" });
-  }
+router.delete("/blogs/:id", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (error) => {
+    if (error) {
+      res.sendStatus(403);
+    } else {
+      try {
+        Blog.deleteOne({ _id: req.params.id });
+        res.status(204).send();
+      } catch {
+        res.status(404);
+        res.send({ error: "blog doesn't exist!" });
+      }
+    }
+  });
 });
 
 module.exports = router;
