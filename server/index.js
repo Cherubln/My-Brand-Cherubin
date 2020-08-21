bc9d703f138222e7621b41a92122f2399c945e16
Cherubin <cherubinkubwayo@gmail.com>
2020-08-21 23:07:07 +0200

    ft-es6: edit index file

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import passport from "./config/passport";
 import session from "express-session";
 const app = express();
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/myBrand", {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/myBrand", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
