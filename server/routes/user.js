9b4375b98640c33348026ba6c3e4be3f396aa390
Cherubin <cherubinkubwayo@gmail.com>
2020-08-25 22:23:05 +0200

    ft-swagger:edit uservalidator file

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,7 @@
 import express from "express";
 const Router = express.Router();
-import passport from "passport";
 import userController from "../controllers/userController";
-
-Router.post("/login", passport.authenticate("local"), userController.logIn);
-
+import validateUser from "../middleware/userValidators";
+Router.post("/login", validateUser, userController.logIn);
 Router.get("/logout", userController.logOut);
-
 export default Router;
